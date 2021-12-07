b7fa2542b22d62aacaabf1a1f32140523b7c1218
Tekie79 <tekiefilm79@gmail.com>
2021-12-07 00:49:48 -0500

    Password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  length = 0;
+  letters = false;
+  numbers = false;
+  symbols = false;
+  password = '';
+
+  onGetLength = (event: any) => {
+    this.length = +event.target.value;
+  };
+
+  onUseLetters = () => {
+    this.letters = !this.letters;
+  };
+
+  onUseNumbers = () => {
+    this.numbers = !this.numbers;
+  };
+  onUseSymbols = () => {
+    this.symbols = !this.symbols;
+  };
+
+  onButtonClick = () => {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%&*+';
+    let validChar = '';
+    if (this.letters) {
+      validChar += letters;
+    }
+    if (this.numbers) {
+      validChar += numbers;
+    }
+    if (this.symbols) {
+      validChar += symbols;
+    }
+
+    let generatedPassowrd = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const randomIndex = Math.floor(Math.random() * validChar.length);
+      generatedPassowrd += validChar[randomIndex];
+    }
+    this.password = generatedPassowrd;
+  };
 }
